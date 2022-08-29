b1a2621d3d3b9edf99e05aef1b3a57caca126ff8
Christin Bartz <bartz@neoskop.de>
2022-08-29 18:03:17 +0200

    implement ItemToComponent

---
+++ src/components.js
@@ -0,0 +1,17 @@
+import Stage from "./components/Stage.astro";
+import Text from "./components/Text.astro";
+import Headline from "./components/Headline.astro";
+import Card from "./components/Card.astro";
+import Column from "./components/Column.astro";
+import Download from "./components/Download.astro";
+import Row from "./components/Row.astro";
+
+export const Components = {
+  Stage,
+  Text,
+  Headline,
+  Card,
+  Column,
+  Download,
+  Row,
+};
